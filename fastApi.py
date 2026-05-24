f575630f81c698b6ab422e8bbdd8b0285ea28398
Yash Pandey <yashpandey1556@gmail.com>
2026-05-24 23:31:35 +0530

    devedev

--- fastApi.py
+++ fastApi.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-  return("Hello this is my first API with FastAPI/This is so stupid")
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+@app.get("/items/{item_name}")
+def read_item(item_name: str | None = None):
+    return {"item_name": item_name}
