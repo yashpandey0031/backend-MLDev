a3c71961b74be4e8e059f35fb0d6615f8192804c
Yash Pandey <yashpandey1556@gmail.com>
2026-05-26 01:35:28 +0530

    created a throw joke showing api

--- fastApi.py
+++ fastApi.py
@@ -1,21 +1,14 @@
 from fastapi import FastAPI
+import random as r
 
 app = FastAPI()
+@app.get("/randomjoke")
+def read_randomjoke():
+    jokes=[
+        "Why don't scientists trust atoms? Because they make up everything!",
+        "Why did the bicycle fall over? Because it was two-tired!",
+        "What do you call a bear with no teeth? A gummy bear!"
+    ]
 
-
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/id/{item_id}")
-def read_item_by_id(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
-
-@app.get("/items/name/{item_name}")
-def read_item_by_name(item_name: str | None = None):
-    return {item_name + " were sold yesterday"}
-
-@app.get("/aboutme")
-def read_aboutme():
-    return {"hello my name is yash pandey and this is my attempt at learning about fast api"}
+    joke = r.choice(jokes)
+    return {"joke of the day isss":joke}
