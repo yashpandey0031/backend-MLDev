5450be3914189dc7b5be1176c84b0c4cc0a28366
Yash Pandey <yashpandey1556@gmail.com>
2026-05-28 17:52:55 +0530

    hallo

---
+++ cat_image.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import httpx
+app = FastAPI()
+
+@app.get("/cat")
+def get_cat_image():
+  cat_image = httpx.get(f"https://api.thecatapi.com/v1/images/search")
+  data = cat_image.json()
+  # only_image = {"Cat_image" : data[0]["url"]}
+  # return {"URL" : only_image}
+  return {"cat_image":data[0]["url"]} #we are using data[0] as the first element of the list 
+
