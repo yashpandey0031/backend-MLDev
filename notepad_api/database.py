81d4fcd71a22248600e8863053f6458fd36e4d1c
Yash Pandey <yashpandey1556@gmail.com>
2026-05-29 22:35:28 +0530

    added notes.db

--- notepad_api/database.py
+++ notepad_api/database.py
@@ -2,3 +2,9 @@ from sqlalchmey import create_engine
 from sqlalchmey.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
+DATABASE_URL = "sqlite:///.notepad_api/notes.db"
+
+engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False,bind = engine)
+Base = declarative_base()
