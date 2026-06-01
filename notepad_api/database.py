e4887ee9fd79a9b3bc5afeceef8e944144d0d2b1
Yash Pandey <yashpandey1556@gmail.com>
2026-06-01 14:57:03 +0530

    moving from sqllite to postgressql

--- notepad_api/database.py
+++ notepad_api/database.py
@@ -1,8 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "sqlite:///./notes.db"
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL,connect_args={"check_same_thread": False})
 
