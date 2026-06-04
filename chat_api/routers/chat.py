5487d5746bc0c12cdbb49c630c4f39767afedb3c
Yash Pandey <yashpandey1556@gmail.com>
2026-06-04 15:06:52 +0530

    adding history and getting that history feature

--- chat_api/routers/chat.py
+++ chat_api/routers/chat.py
@@ -24,13 +24,32 @@ class ConnectionManager:
 
 manager = ConnectionManager()
 
+def get_db():
+  db = SessionLocal()
+  try:
+    yield db
+  finally:
+    db.close()
+
+
+from fastapi import WebSocketDisconnect
 
 @router.websocket("/chat/{username}")
-async def chat_endpoint(websocket: WebSocket, username: str):
+async def chat_endpoint(websocket: WebSocket, username: str, db: Session = Depends(get_db)):
     await manager.connect(websocket)
     try:
         while True:
             data = await websocket.receive_text()
+            new_text = models.Message(username=username, content=data)
+            db.add(new_text)
+            db.commit()
+            db.refresh(new_text)
             await manager.broadcast(f"{username}: {data}")
-    except:
+    except WebSocketDisconnect:
         manager.disconnect(websocket)
+
+
+@router.get("/messages")
+def get_all_messages_by_users(db: Session = Depends(get_db)):
+   all_Data= db.query(models.Message).all()
+   return all_Data
