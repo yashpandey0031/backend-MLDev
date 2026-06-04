bab31bbb88219dc1d3de1dc81cca1a58f9100959
Yash Pandey <yashpandey1556@gmail.com>
2026-06-04 15:19:30 +0530

    Added a interface for easy similation

--- chat_api/main.py
+++ chat_api/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from database import engine
 from routers import chat
-
+from fastapi.middleware.cors import CORSMiddleware
 
 import models
 
@@ -9,4 +9,10 @@ import models
 models.Base.metadata.create_all(bind = engine)
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(chat.router)
