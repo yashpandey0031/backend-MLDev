9d7e43070726bdbab5c6bd15daa489ef5ae26c45
Yash Pandey <yashpandey1556@gmail.com>
2026-06-15 22:38:34 +0530

    idk man

--- chatpdf/main.py
+++ chatpdf/main.py
@@ -1,7 +1,12 @@
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader,RecursiveCharacterTextSplitter
 
 loader = PyPDFLoader("pdf/SystemDesignInterview.pdf")
 
 print(len(loader.load()))
 pages = loader.load()
 print(pages[1].page_content)
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+splitter.split_documents()
+
