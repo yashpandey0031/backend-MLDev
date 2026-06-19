f875ef9c64c7cb6d0636b3f3a6ac85081cd64050
Yash Pandey <yashpandey1556@gmail.com>
2026-06-19 21:47:44 +0530

    understanding about embeddings and storing faiss

--- chatpdf/main.py
+++ chatpdf/main.py
@@ -1,13 +1,23 @@
 from langchain_community.document_loaders import PyPDFLoader #its function is for loading 
 from langchain_text_splitters import RecursiveCharacterTextSplitter #its function is splitting
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.vectorstores import FAISS
+#fullform of faiss
 
 loader = PyPDFLoader("pdf/SystemDesignInterview.pdf")
 
 print(len(loader.load()))
 pages = loader.load()
-print(pages[1].page_content)
+# print(pages[1].page_content)
 
 splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
 
-print(len(splitter.split_documents(pages)))
+chunks =  splitter.split_documents(pages)
 
+embeddings = HuggingFaceEmbeddings(model_name = "sentence-tranformers/all-MiniLM-L6-v2")
+
+results = vectorstore.similarity_search("How does the notification system work?", k=3)
+
+for r in results:
+    print(r.page_content)
+    print("---")
