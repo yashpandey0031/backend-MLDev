ec19f242d204ff710faa1fce189c9a847c4216bb
Yash Pandey <yashpandey1556@gmail.com>
2026-06-20 15:40:45 +0530

    created a simple tool

--- multiToolAgent/agent.py
+++ multiToolAgent/agent.py
@@ -3,6 +3,7 @@ from typing import TypedDict
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage
 from dotenv import load_dotenv
+from langchain_core.tools import tool
 
 load_dotenv()
 
@@ -13,8 +14,15 @@ class AgentState(TypedDict):
     messages: list
 
 
-def call_llm(state: AgentState):
-    response = llm.invoke(state["messages"])
+@tool
+def add(a: int,b:int) -> int:
+    """adds two numbers together.""" #the llm reads this and then understands ok i have to use this and is sent to the model 
+    return a + b
+
+llm_with_tools = llm.bind_tools([add]) #telling the llm that these tools exist 
+
+def call_llm(state: AgentState): #updating the callllm function to use all the tools allowed 
+    response = llm_with_tools.invoke(state["messages"])
     return {"messages": state["messages"] + [response]}
 
 
@@ -25,5 +33,5 @@ graph.add_edge("call_llm", END)
 
 app = graph.compile()
 
-result = app.invoke({"messages": [HumanMessage(content="What is 5 + 5?")]})
+result = app.invoke({"messages": [HumanMessage(content="What is 25 + 35?")]})
 print(result)
