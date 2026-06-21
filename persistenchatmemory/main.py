6383765247eab3463d5947b7d225a53477d8e9d0
Yash Pandey <yashpandey1556@gmail.com>
2026-06-21 20:11:35 +0530

    ss

--- persistenchatmemory/main.py
+++ persistenchatmemory/main.py
@@ -1,9 +1,18 @@
 import chromadb
+
+
+
+embedding_fn = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name="all-MiniLM-L6-v2"
+)
+
 chroma_client = chromadb.PersistentClient(path="./chroma_memory") #connection to the chroma database 
 
 #if a collectino or table called chat_memory already exists open it otherwise create it fresh
-collection = chroma_client.get_or_create_collection(name="chat_memory")
-
+collection = chroma_client.get_or_create_collection(
+    name="chat_memory",
+    embedding_function=embedding_fn
+)
 
 collection.add(
   ids = ["msg1"]
